6aacec87985dc2c6951df4ec8c31f03d63e32b04
nico <nicolas.machicado@bue.edu.ar>
2024-06-14 01:32:43 -0300

    upd: mysql.config

--- db/config/mysql.config.js
+++ db/config/mysql.config.js
@@ -1,9 +1,3 @@
-// const config = {
-//     host: 'sql3.freesqldatabase.com',
-//     user: 'sql3713892',
-//     password: 'fza6WJ5ckp',
-//     database: 'sql3713892'
-// }
 const config = {
     host: process.env.HOST_DB,
     user: process.env.USER_DB,
